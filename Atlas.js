d292bc2069f6d4008639a439cd2160fe20fad73e
Andreas W. Keller <131688400+AKeWeb@users.noreply.github.com>
2023-07-10 19:40:21 +0200

    Update Atlas.js

--- Atlas.js
+++ Atlas.js
@@ -1,3 +1,5 @@
 mongoimport --uri mongodb+srv://awkeller:<Password>@my-movie-app.pshnise.mongodb.net/movieDB --collection movies --type json --file ../exportDB/movies.json
 
-mongoimport --uri mongodb+srv://awkeller:dahre0-baqkef-vurzoZ@my-movie-app.pshnise.mongodb.net/movieDB--collection movies --type json --file ../exportDB/movies.json
+mongoimport --uri mongodb+srv://awkeller:dahre0-baqkef-vurzoZ@my-movie-app.pshnise.mongodb.net/movieDB --collection movies --type json --file ../exportDB/movies.json
+
+mongoimport --uri mongodb+srv://awkeller:dahre0-baqkef-vurzoZ@my-movie-app.pshnise.mongodb.net/movieDB --collection movies --type json --file ../exportDB/movies.json
