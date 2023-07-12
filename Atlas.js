1aa7819b4065e516f1adffaed123fc6e31b7c06e
Andreas W. Keller <131688400+AKeWeb@users.noreply.github.com>
2023-07-12 11:12:03 +0200

    Delete Atlas.js

--- Atlas.js
+++
@@ -1,5 +0,0 @@
-mongoimport --uri mongodb+srv://awkeller:<Password>@my-movie-app.pshnise.mongodb.net/movieDB --collection movies --type json --file ../exportDB/movies.json
-
-mongoimport --uri mongodb+srv://awkeller:dahre0-baqkef-vurzoZ@my-movie-app.pshnise.mongodb.net/movieDB --collection movies --type json --file ../exportDB/movies.json
-
-mongoimport --uri mongodb+srv://awkeller:dahre0-baqkef-vurzoZ@my-movie-app.pshnise.mongodb.net/movieDB --collection users --type json --file ./exportDB/users.json
